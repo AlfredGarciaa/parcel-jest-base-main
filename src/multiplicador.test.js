fee4de15290c160c43c71af616edc3db24f90cd9
Alfred Garcia <alfred.garcia@ucb.edu.bo>
2022-04-11 13:05:24 -0400

    ACTIONS

--- src/multiplicador.test.js
+++ src/multiplicador.test.js
@@ -1,7 +1,13 @@
 import multiplicar from "./multiplicador.js";
 
-describe("Multiplicar", () => {
+describe("Multiplicador", () => {
   it("deberia multiplicar dos numeros", () => {
-    expect(multiplicar(3, 2)).toEqual(6);
+    const resultado = multiplicar(3,2);
+    expect(resultado).toEqual(6); // if resultado == 6 then mostrar mensaje de exito (verde)
+                                  // else mosrar mensaje de falla (rojo)
+  });
+  it("deberia multiplicar dos numeros negativos", () => {
+    const resultado = multiplicar(-5,-8);
+    expect(resultado).toEqual(40)
   });
 });
